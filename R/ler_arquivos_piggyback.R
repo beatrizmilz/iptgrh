fd656cf70ae73f8516853d842df021e33b44ada6
Beatriz Milz <milz.bea@gmail.com>
2024-03-20 13:59:33 -0300

    documentando as funções

--- R/ler_arquivos_piggyback.R
+++ R/ler_arquivos_piggyback.R
@@ -1,21 +1,25 @@
-ler_arquivos_piggyback <- function(base) {
+ler_arquivos_piggyback <- function(base,
+                                   caminho_download = "data-raw/dados_completos_sigrh/") {
 
-  arquivo_baixar <- glue::glue("data-raw/dados_completos_sigrh/{base}.rds")
+  # Cria o caminho do arquivo
+  arquivo_baixar <- glue::glue("{caminho_download}{base}.rds")
 
-  # arquivo_existe <- fs::file_exists(arquivo_baixar)
-
+  # Verifica o tamanho do arquivo.
+  # Isso é útil caso o arquivo já tenha sido baixado
   tamanho_arquivo <- fs::file_size(arquivo_baixar)
 
 
-
+  # Se o arquivo não existir, ou se o tamanho for 0,
+  # prosseguir e baixar o arquivo
   if (tamanho_arquivo == 0 | is.na(tamanho_arquivo)) {
     piggyback::pb_download(
       repo = "beatrizmilz/iptgrh_scraper",
       tag = "dados",
       file = glue::glue("{base}.rds"),
-      dest = "data-raw/dados_completos_sigrh/"
+      dest = caminho_download
     )
   }
 
+  # Ler os dados
   readr::read_rds(arquivo_baixar)
 }
--- R/teste_email_valido.R
+++ R/teste_email_valido.R
@@ -8,5 +8,9 @@
 #' @examples
 #' teste_email_valido("milz.bea@gmail.com")
 teste_email_valido <- function(email) {
-  grepl("\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>", as.character(email), ignore.case = TRUE)
+  grepl(
+    "\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>",
+    as.character(email),
+    ignore.case = TRUE
+  )
 }
